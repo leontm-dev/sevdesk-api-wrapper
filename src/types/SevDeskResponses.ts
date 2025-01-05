5cd605547445675b933ca84fe29ace740c4aa084
LeonTM <lanneken09@gmail.com>
2025-01-05 17:51:32 +0100

    More types for the new functions

--- src/types/SevDeskResponses.ts
+++ src/types/SevDeskResponses.ts
@@ -44,7 +44,7 @@ export type CreateANewClearingAccountBody = {
 export type FindCheckAccountByIdResponse = {
   objects: CheckAccount[];
 };
-export type updateAnExistingCheckAccountBody = {
+export type UpdateAnExistingCheckAccountBody = {
   name: string;
   type: "online" | "offline";
   importType: "CSV" | "MT940" | null;
@@ -54,7 +54,7 @@ export type updateAnExistingCheckAccountBody = {
   autoMapTransactions: number | null;
   accountingNumber: string;
 };
-export type updateAnExistingCheckAccountResponse = {
+export type UpdateAnExistingCheckAccountResponse = {
   id: string;
   objectName: "CheckAccount";
   create: Date;
@@ -73,4 +73,203 @@ export type updateAnExistingCheckAccountResponse = {
   bankServer: string | null;
   autoMapTransactions: string | null;
   accountingNumber: string;
+};
+export type DeleteACheckAccountResponse = {
+  objects: [null];
+};
+export type GetTheBalanceAtAGivenDateResponse = {
+  objects: string;
+};
+export type CheckAccountTransaction = {
+  id: string;
+  objectName: string;
+  create: Date;
+  update: Date;
+  sevClient: {
+    id: string;
+    objectName: "SevClient";
+  };
+  valueDate: Date;
+  entryDate: Date | null;
+  paymtPurpose: string | null;
+  amount: string;
+  payeePayerName: string | null;
+  payeePayerAcctNo: string | null;
+  payeePayerBankCode: string | null;
+  checkAccount: {
+    id: string;
+    objectName: "CheckAccount";
+  };
+  status: "100" | "200" | "300" | "400";
+  sourceTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  targetTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  enshrined: Date;
+};
+export type RetrieveTransactionsResponse = {
+  objects: CheckAccountTransaction[];
+};
+export type CreateANewTransactionBody = {
+  valueDate: Date;
+  entryDate?: Date | null;
+  paymtPurpose?: string | null;
+  amount: number;
+  payeePayerName: string | null;
+  payeePayerAcctNo?: string | null;
+  payeePayerBankCode?: string | null;
+  checkAccount: {
+    id: number;
+    objectName: "CheckAccount";
+  };
+  status: "100" | "200" | "300" | "400";
+  sourceTransaction?: {
+    id: number;
+    objectName: "CheckAccountTransaction";
+  };
+  targetTransaction?: {
+    id: number;
+    objectName: "CheckAccountTransaction";
+  };
+};
+export type CreateANewTransactionResponse = {
+  id: string;
+  objectName: string;
+  create: Date;
+  update: Date;
+  sevClient: {
+    id: string;
+    objectName: "SevClient";
+  };
+  valueDate: Date;
+  entryDate: Date | null;
+  paymtPurpose: string | null;
+  amount: string;
+  payeePayerName: string | null;
+  payeePayerAcctNo: string | null;
+  payeePayerBankCode: string | null;
+  checkAccount: {
+    id: string;
+    objectName: "CheckAccount";
+  };
+  status: "100" | "200" | "300" | "400";
+  sourceTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  targetTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  enshrined: Date;
+};
+export type FindCheckAccountTransactionByIDResponse = {
+  objects: CheckAccountTransaction[];
+};
+export type UpdateAnExistingTransactionBody = {
+  valueDate: Date;
+  entryDate: Date | null;
+  paymtPurpose: string;
+  amount: number | null;
+  payeePayerName: string | null;
+  checkAccount: {
+    id: number;
+    objectName: "CheckAccount";
+  };
+  status: 100 | 200 | 300 | 400;
+  sourceTransaction: {
+    id: number;
+    objectName: "CheckAccountTransaction";
+  } | null;
+  targetTransaction: {
+    id: number;
+    objectName: "CheckAccountTransaction";
+  } | null;
+};
+export type UpdateAnExistingTransactionResponse = {
+  id: string;
+  objectName: string;
+  create: Date;
+  update: Date;
+  sevClient: {
+    id: string;
+    objectName: "SevClient";
+  };
+  valueDate: Date;
+  entryDate: Date | null;
+  paymtPurpose: string | null;
+  amount: string;
+  payeePayerName: string | null;
+  payeePayerAcctNo: string | null;
+  payeePayerBankCode: string | null;
+  checkAccount: {
+    id: string;
+    objectName: "CheckAccount";
+  };
+  status: "100" | "200" | "300" | "400";
+  sourceTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  targetTransaction: {
+    id: string;
+    objectName: "CheckAccountTransaction";
+  };
+  enshrined: Date;
+};
+export type DeleteATransactionResponse = {
+  objects: [null];
+};
+export type EnshrineResponse = {
+  objects: null;
+};
+export type AccountingContact = {
+  id: string;
+  objectName: string;
+  create: Date;
+  update: Date;
+  contact: {
+    id: string;
+    objectName: "Contact";
+  };
+  sevClient: {
+    id: string;
+    objectName: "SevClient";
+  };
+  debitorNumber: string;
+  creditorNumber: string;
+};
+export type RetrieveAccountingContactResponse = {
+  objects: AccountingContact[];
+};
+export type CreateANewAccountingContactBody = {
+  contact: {
+    id: number;
+    objectName: "Contact";
+  };
+  debitorNumber: number | null;
+  creditorNumber: number | null;
+};
+export type CreateANewAccountingContactResponse = {
+  id: string;
+  objectName: string;
+  create: Date;
+  update: Date;
+  contact: {
+    id: string;
+    objectName: "Contact";
+  };
+  sevClient: {
+    id: string;
+    objectName: "SevClient";
+  };
+  debitorNumber: string;
+  creditorNumber: string;
+};
+export type FindAccountingContactByIDResponse = {
+  objects: AccountingContact[];
 };
