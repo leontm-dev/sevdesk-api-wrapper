553fa2ebe32a3b0cf4110b7ee83e507b8851787f
LeonTM <lanneken09@gmail.com>
2025-07-02 14:41:06 +0200

    Add VoucherPos class and response types for voucher position retrieval

---
+++ src/VoucherPos/index.ts
@@ -0,0 +1,34 @@
+// Project-Imports
+
+import { Responder, IResponse } from "../types/Response";
+import { RetrieveVoucherPosResponse } from "./types/response.types";
+
+// Code
+
+export class VoucherPos {
+  private Responder: Responder;
+  constructor(apiKey: string) {
+    this.Responder = new Responder(apiKey, "1");
+  }
+
+  async retrieveVoucherPositions(
+    voucherId: number
+  ): Promise<IResponse<RetrieveVoucherPosResponse>> {
+    return this.Responder.process(
+      "/VoucherPos",
+      {
+        method: "GET",
+      },
+      [
+        {
+          key: "voucher[id]",
+          value: voucherId.toString(),
+        },
+        {
+          key: "voucher[objectName]",
+          value: "Voucher",
+        },
+      ]
+    );
+  }
+}
---
+++ src/VoucherPos/types/response.types.ts
@@ -0,0 +1,9 @@
+// Project-Imports
+
+import { VoucherPosV1 } from "../../types/sevdeskModels";
+
+// Code
+
+export type RetrieveVoucherPosResponse = {
+  objects: VoucherPosV1[];
+};
