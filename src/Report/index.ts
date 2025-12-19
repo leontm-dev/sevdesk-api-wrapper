58228055b59e50fea43653367d4c55347d59bf67
Leon <LAnneken09@gmail.com>
2025-12-19 18:45:03 +0100

    Updated the Report module. Added documentation, improved type safety and made types conform to the new naming conventions

--- src/Report/index.ts
+++ src/Report/index.ts
@@ -2,11 +2,17 @@
 
 import { API } from "../types/common.classes";
 import {
-  ExportContactListSevQuery,
-  ExportInvoiceListSevQuery,
-  ExportOrderListSevQuery,
-  ExportVoucherListSevQuery,
+  ReportModule_Query_ExportContactListSevQuery,
+  ReportModule_Query_ExportInvoiceListSevQuery,
+  ReportModule_Query_ExportOrderListSevQuery,
+  ReportModule_Query_ExportVoucherListSevQuery,
 } from "./types/query.types";
+import {
+  ReportModule_Response_ExportContactListResponse,
+  ReportModule_Response_ExportInvoiceListResponse,
+  ReportModule_Response_ExportOrderListResponse,
+  ReportModule_Response_ExportVoucherListResponse,
+} from "./types/response.types";
 
 // Code
 
@@ -17,12 +23,22 @@ import {
 export class Report {
   constructor(private apiKey: string) {}
 
+  /**
+   * Export invoice list
+   * @link https://api.sevdesk.de/#tag/Report/operation/reportInvoice
+   * @param download
+   * @param view
+   * @param sevQuery
+   * @returns
+   */
   async exportInvoiceList(
     download: boolean,
     view: string,
-    sevQuery: ExportInvoiceListSevQuery
+    sevQuery: ReportModule_Query_ExportInvoiceListSevQuery
   ) {
-    return new API(this.apiKey).request(
+    return new API(
+      this.apiKey
+    ).request<ReportModule_Response_ExportInvoiceListResponse>(
       "/Report/invoiceList",
       {
         download: String(download),
@@ -32,12 +48,23 @@ export class Report {
       { method: "GET" }
     );
   }
+
+  /**
+   * Export order list
+   * @link https://api.sevdesk.de/#tag/Report/operation/reportOrder
+   * @param download
+   * @param view
+   * @param sevQuery
+   * @returns
+   */
   async exportOrderList(
     download: boolean,
     view: string,
-    sevQuery: ExportOrderListSevQuery
+    sevQuery: ReportModule_Query_ExportOrderListSevQuery
   ) {
-    return new API(this.apiKey).request(
+    return new API(
+      this.apiKey
+    ).request<ReportModule_Response_ExportOrderListResponse>(
       "/Report/orderList",
       {
         download: String(download),
@@ -47,11 +74,21 @@ export class Report {
       { method: "GET" }
     );
   }
+
+  /**
+   * Export contact list
+   * @link https://api.sevdesk.de/#tag/Report/operation/reportContact
+   * @param download
+   * @param sevQuery
+   * @returns
+   */
   async exportContactList(
     download: boolean,
-    sevQuery: ExportContactListSevQuery
+    sevQuery: ReportModule_Query_ExportContactListSevQuery
   ) {
-    return new API(this.apiKey).request(
+    return new API(
+      this.apiKey
+    ).request<ReportModule_Response_ExportContactListResponse>(
       "/Report/contactList",
       {
         download: String(download),
@@ -60,11 +97,21 @@ export class Report {
       { method: "GET" }
     );
   }
+
+  /**
+   * Export voucher list
+   * @link https://api.sevdesk.de/#tag/Report/operation/reportVoucher
+   * @param download
+   * @param sevQuery
+   * @returns
+   */
   async exportVoucherList(
     download: boolean,
-    sevQuery: ExportVoucherListSevQuery
+    sevQuery: ReportModule_Query_ExportVoucherListSevQuery
   ) {
-    return new API(this.apiKey).request(
+    return new API(
+      this.apiKey
+    ).request<ReportModule_Response_ExportVoucherListResponse>(
       "/Report/voucherList",
       {
         download: String(download),
--- src/Report/types/query.types.ts
+++ src/Report/types/query.types.ts
@@ -1,65 +1,207 @@
 // Code
 
-export type ExportInvoiceListSevQuery = {
+export type ReportModule_Query_ExportInvoiceListSevQuery = {
+  /**
+   * Limit export
+   */
   limit: number;
+  /**
+   * Model name which is exported
+   */
   modelName: any;
+  /**
+   * SevQuery object name
+   */
   objectName: any;
   filter: {
+    /**
+     * Type of invoices you want to export
+
+1. RE - Rechnung
+2. SR - Stornorechnung
+3. TR - Teilrechnung
+4. AR - Abschlagsrechnung
+5. ER - Endrechnung
+6. WKR - Wiederkehrende Rechnung
+7. MA - Mahnung
+     */
     invoiceType: any[];
+    /**
+     * Start date of the invoice
+     */
     startDate: string;
+    /**
+     * End date of the invoice
+     */
     endDate: string;
+    /**
+     * filters the invoices by contact
+     */
     contact: {
+      /**
+       * ID of the contact
+       */
       id: number;
+      /**
+       * Model name, which is 'Contact'
+       */
       objectName: "Contact";
     };
+    /**
+     * filters the invoices by amount
+     */
     startAmount: number;
+    /**
+     * filters the invoices by amount
+     */
     endAmount: number;
   };
 };
-export type ExportOrderListSevQuery = {
+export type ReportModule_Query_ExportOrderListSevQuery = {
+  /**
+   * Limit export
+   */
   limit: number;
+  /**
+   * Model name which is exported
+   */
   modelName: any;
+  /**
+   * SevQuery object name
+   */
   objectName: any;
   filter: {
+    /**
+     * Type of orders you want to export
+
+1. AN - Angebote
+2. AB - Aufträge
+3. LI - Lieferscheine
+     */
     orderType: "AN" | "AB" | "LI";
+    /**
+     * Start date of the order
+     */
     startDate: string;
+    /**
+     * End date of the order
+     */
     endDate: string;
+    /**
+     * filters the orders by contact
+     */
     contact: {
+      /**
+       * ID of the contact
+       */
       id: number;
+      /**
+       * Model name, which is 'Contact'
+       */
       objectName: "Contact";
     };
+    /**
+     * filters the orders by amount
+     */
     startAmount: number;
+    /**
+     * filters the orders by amount
+     */
     endAmount: number;
   };
 };
-export type ExportContactListSevQuery = {
+export type ReportModule_Query_ExportContactListSevQuery = {
+  /**
+   * Limit export
+   */
   limit: number;
+  /**
+   * Model name which is exported
+   */
   modelName: any;
+  /**
+   * SevQuery object name
+   */
   objectName: any;
   filter: {
+    /**
+     * filters the contacts by zip code
+     */
     zip: number;
+    /**
+     * filters the contacts by city
+     */
     city: string;
     country: {
+      /**
+       * id of the country
+       */
       id: number;
+      /**
+       * Model name, which is 'StaticCountry'
+       */
       objectName: "StaticCountry";
     };
+    /**
+     * export only organisations
+     */
     depth: boolean;
+    /**
+     * export only people
+     */
     onlyPeople: boolean;
   };
 };
-export type ExportVoucherListSevQuery = {
+export type ReportModule_Query_ExportVoucherListSevQuery = {
+  /**
+   * Limit export
+   */
   limit: number;
+  /**
+   * Model name which is exported
+   */
   modelName: any;
+  /**
+   * SevQuery object name
+   */
   objectName: any;
   filter: {
+    /**
+     * Start date of the voucher
+     */
     startDate: string;
+    /**
+     * End date of the voucher
+     */
     endDate: string;
+    /**
+     * Start pay date of the voucher
+     */
+    startPayDate: string;
+    /**
+     * End pay date of the voucher
+     */
     endPayDate: string;
+    /**
+     * filters the vouchers by contact
+     */
     contact: {
+      /**
+       * ID of the contact
+       */
       id: number;
+      /**
+       * Model name, which is 'Contact'
+       */
       objectName: "Contact";
     };
+    /**
+     * filters the vouchers by amount
+     */
     startAmount: number;
+    /**
+     * filters the vouchers by amount
+     */
     endAmount: number;
   };
 };
