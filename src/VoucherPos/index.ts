d0bcb46d46357c8cfdaf615062debd5e4ae9c017
Leon <LAnneken09@gmail.com>
2025-12-19 18:42:10 +0100

    Added documentation to the VoucherPos module. Also improved type safety

--- src/VoucherPos/index.ts
+++ src/VoucherPos/index.ts
@@ -8,6 +8,12 @@ import { RetrieveVoucherPosResponse } from "./types/response.types";
 export class VoucherPos {
   constructor(private apiKey: string) {}
 
+  /**
+   * Retrieve all voucher positions depending on the filters defined in the query.
+   * @link https://api.sevdesk.de/#tag/VoucherPos/operation/getVoucherPositions
+   * @param voucherId Retrieve all vouchers positions belonging to this voucher. Must be provided with voucher[objectName]
+   * @returns
+   */
   async getMany(voucherId?: number) {
     const queryObj: Record<string, string> = {};
     if (voucherId) {
--- src/VoucherPos/types/response.types.ts
+++ src/VoucherPos/types/response.types.ts
@@ -1,7 +1,10 @@
 // Project-Imports
 
+import { VoucherPosV1 } from "./base.v1.types";
+import { VoucherPosV2 } from "./base.v2.types";
+
 // Code
 
 export type RetrieveVoucherPosResponse = {
-  objects: any[];
+  objects: (VoucherPosV1 & VoucherPosV2)[];
 };
