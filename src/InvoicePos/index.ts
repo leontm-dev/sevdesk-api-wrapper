09520bbd32b6d678d657efc6770b97011a758995
Leon <LAnneken09@gmail.com>
2025-12-19 19:16:08 +0100

    Finished the InvoicePos module by adding documentation and improving type safety

--- src/InvoicePos/index.ts
+++ src/InvoicePos/index.ts
@@ -1,12 +1,24 @@
 // Project-Imports
 
 import { API } from "../types/common.classes";
+import { InvoicePosModule_Response_GetMany } from "./types/response.types";
 
 // Code
 
+/**
+ * @link https://api.sevdesk.de/#tag/InvoicePos
+ */
 export class InvoicePos {
   constructor(private apiKey: string) {}
 
+  /**
+   * There are a multitude of parameter which can be used to filter.
+   * @link https://api.sevdesk.de/#tag/InvoicePos/operation/getInvoicePos
+   * @param invoiceId Retrieve all invoices positions with this invoice. Must be provided with invoice[objectName]
+   * @param id Retrieve all InvoicePos with this InvoicePos id
+   * @param partId Retrieve all invoices positions with this part. Must be provided with part[objectName]
+   * @returns
+   */
   async getMany(invoiceId?: number, id?: number, partId?: number) {
     const queryObj: Record<string, string> = {};
     if (invoiceId) {
@@ -19,7 +31,9 @@ export class InvoicePos {
       queryObj["part[objectName]"] = "Part";
     }
 
-    return await new API(this.apiKey).request("/InvoicePos", queryObj, {
+    return await new API(
+      this.apiKey
+    ).request<InvoicePosModule_Response_GetMany>("/InvoicePos", queryObj, {
       method: "GET",
     });
   }
---
+++ src/InvoicePos/types/response.types.ts
@@ -0,0 +1,9 @@
+// Imports
+
+import type { InvoicePos } from "./base.types";
+
+// Code
+
+export type InvoicePosModule_Response_GetMany = {
+  objects: InvoicePos[];
+};
